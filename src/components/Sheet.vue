8d535c8ef7ee0042ddd5d1e81df82c58f55611ca
ImnMuqri <imnmuqri@gmail.com>
2024-06-09 21:13:05 +0800

    Done

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -17,7 +17,7 @@
             <v-col class="text-center text-md-right text-sm-right">
               <v-dialog
                 transition="dialog-bottom-transition"
-                class="w-1/2 h-full"
+                class="w-full h-full"
               >
                 <template v-slot:activator="{ props: activatorProps }">
                   <v-btn
